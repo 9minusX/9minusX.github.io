edb9153e2e9850abbb7f0cd761bea68ceb38c4d6
Kevin J.C <ui.cheung@gmail.com>
2024-11-08 22:10:34 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,7 +10,7 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Yu's Running',
+  siteTitle: 'Yu Running',
   siteUrl: 'https://running.yusir.im',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Running record and blog',
